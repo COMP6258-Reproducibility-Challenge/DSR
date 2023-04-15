0fff9287879ea1bf134767989e2051fbba4f7720
dz1u19 <dz1u19@soton.ac.uk>
2023-04-15 13:31:03 +0100

    Simplified add_node, started node_mask

--- environment/Expr.py
+++ environment/Expr.py
@@ -32,23 +32,37 @@ class Expr():
                 The index of the node in the library
         """
         node_to_add = self.library.get_node(node_index)
+        self.node_list.append(node_to_add)
 
+        #add the current node as a child of a node on the stack
         if len(self.node_list) > 0:
             self.stack.pop().add_child(node_to_add)
 
-        self.node_list.append(node_to_add)
-        last_node = self.node_list[-1]
-        if last_node.remaining_children() == 2:
-            self.stack.append(last_node)
-            self.stack.append(last_node)
-        elif last_node.remaining_children() == 1:
-            self.stack.append(last_node)
+        #add the current node to the stack for every child space it has
+        for _ in range(node_to_add.remaining_children()):
+            self.stack.append(node_to_add)
 
+    
     def valid_nodes_mask(self):
         """
         Returns a mask (list of boolean values) of the same shape as the library where a True reflects that 
         the corresponding node is valid and could be added next, and a False reflects that that node is an invalid
         addition. This will be needed to zero out the output probabilities of invalid nodes.
         """
-        # len(self.node_list) + len(self.stack) < 4 and len(self.stack) < 2 remove terminals
+
+        mask = [True for _ in range(len(self.library))]
+
+        #make sure the tree is 4 or more nodes long
+        if len(self.node_list) + len(self.stack) < 4 and len(self.stack) < 2:
+            #TODO disallow adding terminals
+            pass
+
+        #make sure the tree is 30 or less nodes long
+        else:
+            if len(self.node_list) + len(self.stack) > 28:
+                #TODO disallow adding binary operators
+                pass
+            if len(self.node_list) + len(self.stack) > 29:
+                #TODO disallow adding unary operators
+                pass
         pass
