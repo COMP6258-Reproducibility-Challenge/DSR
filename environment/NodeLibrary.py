54c6a79dae3ae27670e77b35a37000e63f072cc5
dz1u19 <dz1u19@soton.ac.uk>
2023-04-17 17:20:11 +0100

    Added get_node_int to Library

--- environment/NodeLibrary.py
+++ environment/NodeLibrary.py
@@ -1,3 +1,5 @@
+from nodes import Node
+
 class Library():
     """
     Represents the set of operators that can be included in an equation
@@ -16,3 +18,10 @@ class Library():
     
     def get_node(self, node_index: int):
         return self.nodes[node_index].duplicate()
+
+    def get_node_int(self, node: Node):
+        for i in range(len(self.nodes)):
+            lib_node = self.nodes[i]
+            if node.__class__.__name__ == lib_node.__class__.__name__:
+                return i
+        return -1
