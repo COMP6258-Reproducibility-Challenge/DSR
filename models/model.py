6681bd8182fc4019eff3043fe2eace9e7f6f1153
Nikol Stoeva <nikolstst00@gmail.com>
2023-04-22 16:48:05 +0100

    Model is finished

--- models/model.py
+++ models/model.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from torch.distributions.categorical import Categorical
+import numpy as np
 
 
 class Regressor(nn.Module):
@@ -23,7 +24,9 @@ class Regressor(nn.Module):
         super().__init__()
 
         self.output_size = output_size
-        self.lstm = nn.LSTM(embedding_size, hidden_size)    # one LSTM layer by default
+        self.embedding = torch.empty(output_size, embedding_size)
+        nn.init.kaiming_normal_(self.embedding)
+        self.lstm = nn.LSTM(embedding_size, hidden_size)
 
     def forward(self, x, hidden_state):
         """
@@ -38,15 +41,11 @@ class Regressor(nn.Module):
             Tuple of probability distribution and hidden state
         """
 
-        LSTM_output, (last_hidden_state, _) = self.lstm(x, hidden_state)
-
-        # maybe just softmax if it makes any difference
-        softmax = nn.LogSoftmax(dim=self.output_size)
+        LSTM_output, last_hidden_state = self.lstm(self.embedding[x], hidden_state)
+        softmax = nn.Softmax(dim=self.output_size)
         prob_dist = softmax(LSTM_output)
 
-        # might have to concat the last_hidden_state with the other value of the tuple but i think this is what we want
         return prob_dist, last_hidden_state
-
 
     def sample_action(self, x, hidden_state, mask):
         """
@@ -63,13 +62,16 @@ class Regressor(nn.Module):
             Tuple of the generated action, the hidden state, the log probability of choosing that action, and the entropy of the
             generated categorical distribution
         """
+
         action_dist, prev_hidden_state = self.forward(x, hidden_state)
         # Use mask to zero out probabilities here
         # those are of the invalid nodes
+        action_dist *= torch.as_tensor(mask).reshape(action_dist.shape)
+        action_dist /= action_dist.sum()
+
         dist = Categorical(action_dist)
         action = dist.sample()
         log_prob = dist.log_prob(action)
-        # Not sure if we should be calculating entropy before or after applying the mask...
-        # Or if that even makes a difference
+
         entropy = dist.entropy()
         return action, hidden_state, log_prob, entropy
