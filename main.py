613727e85777b9f858f9f720a76a56da4983d017
Samuel Lennard <sl2g19@soton.ac.uk>
2023-04-26 14:25:17 +0100

    Updated main

--- main.py
+++ main.py
@@ -12,22 +12,49 @@ from environment.nodes.Node import *
 nodes_list = [Add, Sub, Mult, Div, Sin, Cos, Log, Exp, X]#, Y]#, Const]
 library = Library(nodes_list)
 
-embedding_size = 64
-hidden_size = 64
-model = Regressor(embedding_size, hidden_size, library.get_size())
+embedding_size = 128
+hidden_size = 256
+model = Regressor(embedding_size, hidden_size, library.get_size()).to(device="cuda")
 
 expression = ExprTree(library)
+# x^4 + x^3 + x^2 + x
+# expression.add_node(0)
+# expression.add_node(0)
+# expression.add_node(0)
+# expression.add_node(2)
+# expression.add_node(2)
+# expression.add_node(2)
+# expression.add_node(8)
+# expression.add_node(8)
+# expression.add_node(8)
+# expression.add_node(8)
+# expression.add_node(2)
+# expression.add_node(2)
+# expression.add_node(8)
+# expression.add_node(8)
+# expression.add_node(8)
+# expression.add_node(2)
+# expression.add_node(8)
+# expression.add_node(8)
+# expression.add_node(8)
+# sin(x) + sin(x + x^2)
 expression.add_node(0)
+expression.add_node(4)
+expression.add_node(8)
+expression.add_node(4)
 expression.add_node(0)
-expression.add_node(2)
-expression.add_node(2)
-expression.add_node(8)
-expression.add_node(8)
 expression.add_node(8)
 expression.add_node(2)
 expression.add_node(8)
 expression.add_node(8)
-expression.add_node(8)
+# sin(x) + sin(y^2)
+# expression.add_node(0)
+# expression.add_node(4)
+# expression.add_node(8)
+# expression.add_node(4)
+# expression.add_node(2)
+# expression.add_node(9)
+# expression.add_node(9)
 
 
 target_expr = Expr(library, expression.node_list)
@@ -37,5 +64,4 @@ dataset = Dataset(target_expr, numpoints=20)
 env = SymbolicRegressionEnv(library, dataset, hidden_size)
 
 learner = Learner(env, model, epochs=2000, batch_size=1000)
-# torch.autograd.set_detect_anomaly(True)
 losses, rewards, best_expr, max_reward = learner.train()
